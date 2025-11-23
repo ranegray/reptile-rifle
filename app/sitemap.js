5675e10c8862a5af8575718ccc16d73301d0e988
Rane Gray <ranegray830@gmail.com>
2025-11-23 08:45:25 -0700

    seo icons and files

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    { url: 'https://golemrobot.com', lastModified: new Date() },
+    { url: 'https://golemrobot.com/about', lastModified: new Date() },
+    { url: 'https://golemrobot.com/specs', lastModified: new Date() },
+    { url: 'https://golemrobot.com/pilot', lastModified: new Date() },
+    { url: 'https://golemrobot.com/case-study', lastModified: new Date() },
+    { url: 'https://golemrobot.com/contact', lastModified: new Date() },
+  ]
+}
